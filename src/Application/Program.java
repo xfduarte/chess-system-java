448781a348801e35dcbc31b992d6bf811e24d488
xfduarte <duarte.brusque@gmail.com>
2024-03-22 16:29:41 -0300

    Checkmate logic

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<ChessPiece>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -46,7 +46,8 @@ public class Program {
 			}
 			
 		}
-		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
